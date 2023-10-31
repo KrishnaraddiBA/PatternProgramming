c237c006bad276812252354e779586003a70c7a4
krishnaraddi <krishnaraddi.alagawadi1997@gmail.com>
2023-10-31 13:55:30 +0530

    patterns

--- A.java
+++ A.java
@@ -98,14 +98,27 @@ public class A {
 		
 		//Pattern 8
 		
+//		for(int i=1;i<=5;i++) {
+//			for(int j=5;j>=i;j--) {
+//				System.out.print(" ");
+//			}
+//			for(int k=1;k<=i;k++) {
+//				System.out.print("*");
+//			}
+//			for(int l=1;l<=i;l++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+		
+		//Pattern9
+		
+		
 		for(int i=1;i<=5;i++) {
-			for(int j=5;j>=i;j--) {
+			for(int k=1;k<=i;k++) {
 				System.out.print(" ");
 			}
-			for(int k=1;k<=i;k++) {
-				System.out.print("*");
-			}
-			for(int l=1;l<=i;l++) {
+			for(int j=1;j<=11-(2*i);j++) {
 				System.out.print("*");
 			}
 			System.out.println();
